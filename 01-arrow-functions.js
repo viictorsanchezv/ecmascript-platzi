79682a34960e38d7115717e9a8cf2895c930a72f
Victor Sanchez <ingsanchez423@gmail.com>
2023-06-14 23:17:52 -0400

    second commit

---
+++ 01-arrow-functions.js
@@ -0,0 +1,8 @@
+function square (num){
+    return num * num;
+}
+
+const square = (num) => {
+    return num * num;
+}
+
---
+++ 03-strings.js
@@ -0,0 +1,13 @@
+let hello = "Hello";
+let world = "World";
+let helloWorld = hello+" "+world;
+//console.log(helloWorld);
+
+//Template literals
+let helloWorld2 = `${hello} ${world}`;
+console.log(helloWorld2);
+
+//MultiLine String
+let lorem = `esto es un frase epica
+continuacion de la frase`;
+console.log(lorem);
